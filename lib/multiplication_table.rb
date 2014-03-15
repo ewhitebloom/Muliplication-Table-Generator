4768a3ad6699d6a53560535240d290d4954d9d74
ewhitebloom <ewhitebloom@gmail.com>
2014-03-14 23:10:47 -0400

    trimmed

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -1,7 +1,5 @@
 def multiplication_table(vertical,horizontal)
   table = []
-  (1..vertical).each do |num|
-    table << (1..horizontal).map { |number| number * num }
-  end
+  (1..vertical).each { |num| table << (1..horizontal).map { |number| number * num } }
   table.each { |row| puts row.join("\t") }
 end
